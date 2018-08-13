5e2122cc74c80d3f32015c0f6def6f84d4020c2e
Ramesh Advani <rvadvani3@gmail.com>
2018-08-13 13:18:31 +0530

    Add files via upload

---
+++ JS-JQuery/java-script.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Demo for Java Script</title>
+</head>
+<body>
+
+
+<script type="text/javascript">
+	var date = new Date();
+	console.log(date);
+</script>
+</body>
+</html>
