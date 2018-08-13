c65679c60998e2c0b544b2c8ca55c5eed7a17cf3
Ramesh Advani <rvadvani3@gmail.com>
2018-08-13 13:34:36 +0530

    Update java-script.php

--- JS-JQuery/java-script.php
+++ JS-JQuery/java-script.php
@@ -6,9 +6,30 @@
 <body>
 
 
-<script type="text/javascript">
-	var date = new Date();
-	console.log(date);
-</script>
+<script>
+	// check box true or false in JS
+	if(document.getElementById('ID').checked == true) {
+		console.log('true');
+	} else {
+		console.log('false');
+	}
+	
+	// check box true or false in JQuery
+	if($("#ID").is(':checked')){
+		console.log('true');
+	} else {
+		console.log('false');
+	}
+		
+	// document get ready
+    	$( document ).ready(function() {
+        	console.log( "document loaded" );
+    	});
+ 	
+	// onload function
+    	$( window ).on( "load", function() {
+        	console.log( "window loaded" );
+    	});
+    </script>
 </body>
 </html>
