ada3f524abd0cbc1736047ccb0fa7a9e09cab840
Ramesh Advani <rvadvani3@gmail.com>
2018-08-14 12:38:30 +0530

    Update code-sample.php

--- Codeigniter/code-sample.php
+++ Codeigniter/code-sample.php
@@ -4,3 +4,8 @@
 // Update Query
 $this->db->update('mytable', $data, array('id' => $id));
 
+// Join Query
+$this->db->select('users.usrID, users_profiles.usrpID')
+         ->from('users')
+         ->join('users_profiles', 'users.usrID = users_profiles.usrpID')
+         ->get();
