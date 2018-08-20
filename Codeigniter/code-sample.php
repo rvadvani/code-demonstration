90a2aa32ae8de564e5d57049d9a968057fc333fc
Ramesh Advani <rvadvani3@gmail.com>
2018-08-20 11:25:02 +0530

    Update code-sample.php

--- Codeigniter/code-sample.php
+++ Codeigniter/code-sample.php
@@ -9,3 +9,4 @@ $this->db->select('users.usrID, users_profiles.usrpID')
          ->from('users')
          ->join('users_profiles', 'users.usrID = users_profiles.usrpID')
          ->get();
+$this->db->where('id', $id)->delete('users');
